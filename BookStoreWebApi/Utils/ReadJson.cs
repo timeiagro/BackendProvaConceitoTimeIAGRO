fe2d0958f92eb444fb9384234c54a490a061a4bb
Caio Torres <caiotorres99@outlook.com>
2024-03-04 21:57:36 -0400

    Removendo comentários

--- BookStoreWebApi/Utils/ReadJson.cs
+++ BookStoreWebApi/Utils/ReadJson.cs
@@ -12,12 +12,7 @@ namespace BookStoreWebApi.Utils
             var buildDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var filePath = buildDir + @"\Json\books.json";
             string json = File.ReadAllText(filePath);
-            // return System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json);
 
-            //var json = File.ReadAllText(@"C:\Users\caiot\source\repos\BackendProvaConceitoTimeIAGRO\Json\books.json");
-            //string json = @"./books.json";
-
-            //var jsonString = File.ReadAllLines(json);
             List<Book> books = JsonConvert.DeserializeObject<List<Book>>(json);
 
             return books;
